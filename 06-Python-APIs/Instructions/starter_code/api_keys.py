b37b038af6b872949ac4a022b82366774719f5a5
Kumba Ye <kumba@Kumbas-MacBook-Pro.local>
2020-08-06 21:03:12 -0400

    calculated Lon and Lat

--- 06-Python-APIs/Instructions/starter_code/api_keys.py
+++ 06-Python-APIs/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "72102a87804b5e2c0b558b44e0652cf3"
+weather_api_key = "36d27378545f7daafa9d3ff4e9ef2e7a"
 
 # Google API Key
 g_key = "AIzaSyCTwAzrVC2_TC3mOJwMuqn2PTf-PTT3Prw"
